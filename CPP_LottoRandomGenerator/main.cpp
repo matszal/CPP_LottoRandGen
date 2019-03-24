426cf5bc4c2b1d4a74c70e956c2680437c8d84fa
matszal <mszalkowski2011@gmail.com>
2019-03-24 17:22:53 +0000

    Minor changes
    Added 2D vector to store random generated numbers.

--- CPP_LottoRandomGenerator/main.cpp
+++ CPP_LottoRandomGenerator/main.cpp
@@ -9,29 +9,59 @@
 #include <iostream>
 #include <random>
 #include <ctime>
+#include <vector>
+#include <algorithm>
 
 
-int main(int argc, const char * argv[]) {
-  
+int main(int argc, const char * argv[])
+{
+  const int MAX_NUMBERS = 47;
   int numOfLines;
-  const int MAX_NUMBERS = 6;
+  std::vector<std::vector<int>> numbers;
+  std::vector<std::vector<int>> selected;
   
-  std::default_random_engine engine( static_cast<unsigned int>(time(0)));
-  std::uniform_int_distribution<unsigned int> randomInt(1, 47);
-  
-  
-  std::cout<<"Sexy lotto number generator with C++11 rand style"<<std::endl;
+  std::random_device rd;
+  std::mt19937 g(rd());
+
   std::cout<<"How many lines ya want? "<<std::endl;
-  
   std::cin>>numOfLines;
   
-  for (int lines = 0; lines != numOfLines; ++lines)
+  // fill vectors with numbers 1-47
+  for (int i = 0; i != numOfLines; i++)
   {
-    for (int nos = 0; nos != MAX_NUMBERS; ++nos)
+    std::vector<int> temp;
+    for (int j = 1; j <= MAX_NUMBERS; j++)
     {
-        std::cout << randomInt(engine)<<" ";
+      temp.push_back(j);
+    }
+    numbers.push_back(temp);
+  }
+  
+  // shuffle numbers
+  for (auto i = 0; i < numbers.size(); i ++)
+  {
+    std::shuffle(numbers[i].begin(), numbers[i].end(), g);
+  }
+  
+  // fill 6 lucky numbers
+  for (int i = 0; i <= numOfLines; i++)
+  {
+    std::vector<int> temp2;
+    temp2.insert(temp2.end(), numbers[i].begin(), numbers[i].end()-41);
+    selected.push_back(temp2);
+  }
+  
+  // print 6 numbers for debugging
+  for (int i = 0; i != numOfLines; ++i)
+  {
+    // sort
+    std::sort(selected[i].begin(), selected[i].end());
+    for (int j = 0; j != selected[i].size(); ++j)
+    {
+      std::cout << selected[i][j]<<" ";
     }
     std::cout<<"\n";
   }
+  
   return 0;
 }
